dbd0d58e4f2868608c5b77cf553cf9744f9c03da
Gaston Amigo <gastonamigo@gmail.com>
2023-03-07 20:03:16 -0300

    se agrego el chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,40 @@
-console.log("hello world");
+const socket = io();
+
+let username;
+
+Swal.fire({
+  title: "Identifícate",
+  input: "text",
+  text: "Ingresa tu nombre de usuario",
+  inputValidator: (value) => {
+    return !value && "Es obligatorio introducir un nombre de usuario";
+  },
+  allowOutsideClick: false,
+}).then((result) => {
+  username = result.value;
+});
+
+const chatInput = document.getElementById("chat-input");
+chatInput.addEventListener("keyup", (ev) => {
+  if (ev.key === "Enter") {
+    const inputMessage = chatInput.value;
+
+    if (inputMessage.trim().length > 0) {
+      socket.emit("chat-message", { username, message: inputMessage });
+
+      chatInput.value = "";
+    }
+  }
+});
+
+const messagesPanel = document.getElementById("messages-panel");
+socket.on("messages", (data) => {
+  console.log(data);
+  let messages = "";
+
+  data.forEach((m) => {
+    messages += `<b>${m.username}:</b> ${m.message}</br>`;
+  });
+
+  messagesPanel.innerHTML = messages;
+});
